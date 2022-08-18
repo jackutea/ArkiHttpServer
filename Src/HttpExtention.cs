564550ac078d21d1bedc3213f95e7a5755cc1e4d
杰克有茶 <chenwansal1@163.com>
2022-08-18 16:50:59 +0800

    <feature> add: sync send / read

--- Src/HttpExtention.cs
+++ Src/HttpExtention.cs
@@ -14,13 +14,34 @@ namespace JackFrame.HttpNS {
             ArraySegment<byte> arr = new ArraySegment<byte>(buffer, 0, count);
             return arr;
         }
+
+        public static ArraySegment<byte> ReadBuffer(this HttpListenerRequest req, byte[] buffer) {
+            int count = req.InputStream.Read(buffer, 0, buffer.Length);
+            ArraySegment<byte> arr = new ArraySegment<byte>(buffer, 0, count);
+            return arr;
+        }
         
         // Response
+        public static void SendUTF8String(this HttpListenerResponse res, string content) {
+            byte[] buffer = Encoding.UTF8.GetBytes(content);
+            res.ContentLength64 = buffer.Length;
+            Stream output = res.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+
         public static async Task SendUTF8StringAsync(this HttpListenerResponse res, string content) {
             byte[] buffer = Encoding.UTF8.GetBytes(content);
             res.ContentLength64 = buffer.Length;
             Stream output = res.OutputStream;
             await output.WriteAsync(buffer, 0, buffer.Length);
+            output.Close();
+        }
+
+        public static void SendBuffer(this HttpListenerResponse res, byte[] buffer) {
+            res.ContentLength64 = buffer.Length;
+            Stream output = res.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
             output.Close();
         }
 
